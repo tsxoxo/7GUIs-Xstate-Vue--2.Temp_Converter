28ba17f410073fb048ae1df3a529d3eea395a191
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2024-02-01 12:56:55 +0000

    style

--- src/TempConverter.vue
+++ src/TempConverter.vue
@@ -14,54 +14,16 @@ const { snapshot, send } = useMachine(tempMachine, {
 </script>
 
 <template>
-  <div>
-    <label for="fahrenheit">Fahrenheit</label>
-    <input @input="(event) => send({ type: 'changeF', value: (event!.target as HTMLInputElement)!.value })" type="text"
+  <div class="temp-module">
+    <input placeholder="----"
+      @input="(event) => send({ type: 'changeF', value: (event!.target as HTMLInputElement)!.value })" type="text"
       :value="snapshot.context.fahrenheit" id="fahrenheit" size="10" />
-    <label for="celsius">Celsius</label>
-    <input @input="(event) => send({ type: 'changeC', value: (event!.target as HTMLInputElement)!.value })"
+    <label for="fahrenheit">Fahrenheit</label>
+  </div>
+  <div class="temp-module">
+    <input placeholder="----"
+      @input="(event) => send({ type: 'changeC', value: (event!.target as HTMLInputElement)!.value })"
       :value="snapshot.context.celsius" type="text" id="celsius" size="10" />
+    <label for="celsius">Celsius</label>
   </div>
 </template>
-
-<style scoped>
-.step {
-  padding: 2rem;
-  background: white;
-  border-radius: 1rem;
-  box-shadow: 0 0.5rem 1rem #0001;
-  width: 75vw;
-  max-width: 40rem;
-}
-
-.feedback>.close-button {
-  position: absolute;
-  top: 0;
-  right: 0;
-  z-index: 1;
-}
-
-.button {
-  appearance: none;
-  color: white;
-  border: none;
-  padding: 1rem 1.5rem;
-  border-radius: 0.25rem;
-  font-size: inherit;
-  font-weight: bold;
-  margin-right: 1rem;
-  background-color: var(--color-primary);
-}
-
-.button:disabled {
-  cursor: not-allowed;
-  opacity: 0.5;
-}
-
-.close-button {
-  appearance: none;
-  background: transparent;
-  border: none;
-  padding: 1rem;
-}
-</style>
